eebe464957daed61a9392f44250bfba8e25becbf
yaoqifa <yaoqifa07330@hellobike.com>
2019-10-24 16:37:28 +0800

    cli

--- commands/init.js
+++ commands/init.js
@@ -9,7 +9,7 @@ const path = require('path')
 const option =  program.parse(process.argv).args[0]
 const defaultName = typeof option === 'string' ? option : 'react-project'
 const tplList = require(`${__dirname}/../templates`)
-const tplLists = Object.keys(tplList) || [];
+const tplLists = Object.keys(tplList) || []
 const question = [
   {
     type: 'input',
@@ -63,12 +63,12 @@ const question = [
   }
 ]
 module.exports = prompt(question).then(({name, template, description, author}) => {
-  const projectName = name;
-  const templateName = template;
-  const gitPlace = tplList[templateName]['place'];
-  const gitBranch = tplList[templateName]['branch'];
-  const spinner = ora('Downloading please wait...');
-  spinner.start();
+  const projectName = name
+  const templateName = template
+  const gitPlace = tplList[templateName]['place']
+  const gitBranch = tplList[templateName]['branch']
+  const spinner = ora('Downloading please wait...')
+  spinner.start()
   download(`${gitPlace}${gitBranch}`, `./${projectName}`, (err) => {
     if (err) {
       console.log(chalk.red(err))
@@ -76,20 +76,20 @@ module.exports = prompt(question).then(({name, template, description, author}) =
     }
     fs.readFile(`./${projectName}/package.json`, 'utf8', function (err, data) {
       if(err) {
-        spinner.stop();
-        console.error(err);
-        return;
+        spinner.stop()
+        console.error(err)
+        return
       }
-      const packageJson = JSON.parse(data);
-      packageJson.name = name;
-      packageJson.description = description;
-      packageJson.author = author;
-      var updatePackageJson = JSON.stringify(packageJson, null, 2);
+      const packageJson = JSON.parse(data)
+      packageJson.name = name
+      packageJson.description = description
+      packageJson.author = author
+      const updatePackageJson = JSON.stringify(packageJson, null, 2)
       fs.writeFile(`./${projectName}/package.json`, updatePackageJson, 'utf8', function (err) {
         if(err) {
-          spinner.stop();
-          console.error(err);
-          return;
+          spinner.stop()
+          console.error(err)
+          return
         } else {
           spinner.stop();
           console.log(chalk.green('project init successfully!'))
